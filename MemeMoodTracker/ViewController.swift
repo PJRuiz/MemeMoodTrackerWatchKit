f0066409d5f186716069d0f826b8a1f04ddcf02a
Pedro Ruíz <pedro.ruiz.diaz@me.com>
2015-12-05 18:06:42 +0900

    Initial Commit

---
+++ MemeMoodTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMoodTracker
+//
+//  Created by Pedro Ruíz on 12/5/15.
+//  Copyright © 2015 Pedro Ruíz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
